9b705f4cbc1753c039e39dc1bb32800e09ead32b
miranda lin <miranda.lin@online-pajak.com>
2021-06-05 18:18:50 +1000

    setup project

---
+++ src/main/src/kotlin/au/mir/transaction/Main.kt
@@ -0,0 +1,12 @@
+package kotlin.au.mir.transaction
+
+import java.util.logging.Level.INFO
+import java.util.logging.Logger
+
+val logger: Logger = Logger.getLogger("main")
+
+fun main(args: Array<String>) {
+
+    logger.log(INFO,"Processing Transactions")
+}
+
---
+++ src/main/src/kotlin/au/mir/transaction/Parser.kt
@@ -0,0 +1,4 @@
+package kotlin.au.mir.transaction
+
+object Parser {
+}
